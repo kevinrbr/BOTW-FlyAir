9bc1091c0eb13cbd09552f9c36477f4f05afd8ba
kevinrbr <kevinrbr16@gmail.com>
2025-05-10 00:47:06 +0200

    wip composant pastille bleu

---
+++ src/components/CircleIcon.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps<{
+  iconName: string;
+  label: string;
+}>();
+</script>
+
+<template>
+  <div class="circle-icon container">
+    <div class="circle-icon__icon">I</div>
+    <span class="circle-icon__text">{{ label }}</span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.circle-icon {
+    &__icon {
+        background-color: var(--secondary);
+        height: 40px;
+        width: 40px;
+        border-radius: 100%;
+    }
+    &__text {
+        font-size: 14px;
+        color: var(--color-text)
+    }
+}
+</style>
